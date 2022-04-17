07dd79a8f0c4bb2acc55f0b403a7c804db1f7109
Dmytro Parfeniuk <parfeniukink@gmail.com>
2022-04-17 23:14:44 +0300

    Provide code quality

--- src/costs/__init__.py
+++ src/costs/__init__.py
@@ -1,4 +1,4 @@
+from costs.errors import *  # noqa
 from costs.keyboards import *  # noqa
 from costs.models import *  # noqa
-from costs.errors import *  # noqa
 from costs.services import *  # noqa

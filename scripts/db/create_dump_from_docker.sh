73bf7a5bfd9ee42a100e1b1c761cbb83df632ab2
Dmytro Parfeniuk <parfeniukink@gmail.com>
2022-04-24 13:18:06 +0300

    Update script

--- scripts/db/create_dump_from_docker.sh
+++ scripts/db/create_dump_from_docker.sh
@@ -1,1 +1,1 @@
-docker-compose exec postgres pg_dump -U postgres family_budget > dump.sql
+docker-compose -f /root/family_budget_bot/docker-compose.yaml exec postgres pg_dump -U postgres family_budget > /root/family_budget_bot/dump.sql

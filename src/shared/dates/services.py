0b6ceac6d02e11a34df2deae9ed317f77dbabd4d
Dmytro Parfeniuk <parfeniukink@gmail.com>
2022-05-02 17:02:43 +0300

    Fix: no current month in analytics if prev month is behind and bot wasn't restarted

--- src/shared/dates/services.py
+++ src/shared/dates/services.py
@@ -1,6 +1,8 @@
 from datetime import date, timedelta
 from typing import Iterable, Optional
 
+from loguru import logger
+
 from config import database
 from shared.errors import UserError
 from shared.sequences import without_duplicates
@@ -20,43 +22,63 @@ class DatesCache(type):
         except IndexError:
             return None
 
+    @classmethod
+    def set_dates(cls) -> Optional[tuple[date, date]]:
+        dates = cls.get_dates()
+
+        if not dates:
+            return None
+
+        first_date = dates[0]
+        last_date = dates[1]
+
+        setattr(cls, "FIRST_DATE", first_date)
+        setattr(cls, "LAST_DATE", last_date)
+
+        logger.info("Update dates cache")
+
+        return dates
+
     def __getattr__(cls, attr):
         if attr == "FIRST_DATE" or attr == "LAST_DATE":
-            dates = cls.get_dates()
+            dates = cls.set_dates()
 
             if not dates:
                 return None
 
-            first_date = dates[0]
-            last_date = dates[1]
-
-            setattr(cls, "FIRST_DATE", first_date)
-            setattr(cls, "LAST_DATE", last_date)
-
             if attr == "FIRST_DATE":
-                return first_date
+                return dates[0]
 
             if attr == "LAST_DATE":
-                return last_date
+                return dates[1]
 
         raise AttributeError(attr)
 
 
 class DatesService(metaclass=DatesCache):
     @classmethod
+    def __get_monthes_in_range(cls, first_date: date, last_date: date, date_format: str = "%Y-%m") -> list[str]:
+        results = []
+        while last_date > first_date:
+            results.append(last_date.strftime(date_format))
+            last_date -= timedelta(days=last_date.day)
+
+        return results
+
+    @classmethod
     def get_formatted_dates(cls, date_format: str = "%Y-%m") -> Iterable:
         """Return the list of dates from the first saved cost to today in format YEAR-MONTH"""
+
         if not cls.FIRST_DATE or not cls.LAST_DATE:
             raise UserError("Currently we do not have any costs in database")
 
         if all([cls.FIRST_DATE.year == cls.LAST_DATE.year, cls.FIRST_DATE.month == cls.LAST_DATE.month]):
             return list(cls.FIRST_DATE.strftime(date_format))
 
-        data = [
-            (cls.FIRST_DATE + timedelta(_)).strftime(date_format)
-            for _ in range(
-                (cls.LAST_DATE - cls.FIRST_DATE).days,
-            )
-        ]
+        data: list[str] = cls.__get_monthes_in_range(cls.FIRST_DATE, cls.LAST_DATE, date_format)
+
+        if not date.today().strftime(date_format) in data:
+            DatesCache.set_dates()
+            data: list[str] = cls.__get_monthes_in_range(cls.FIRST_DATE, cls.LAST_DATE, date_format)
 
         return without_duplicates(reversed(data))

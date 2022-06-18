62759951c63db35af4dedac5fe449704f371dfbf
parfeniukink <45270625+parfeniukink@users.noreply.github.com>
2022-06-19 01:58:06 +0300

    Update income sources validation (#36)

--- src/configurations/validators.py
+++ src/configurations/validators.py
@@ -5,7 +5,6 @@ from configurations.domain import (
     Configurations,
     ConfigurationsStorage,
 )
-from shared.messages import BOLD, CODE
 
 
 def validate_new_dates_keyboard_len(value: str) -> None:
@@ -15,28 +14,12 @@ def validate_new_dates_keyboard_len(value: str) -> None:
         raise ConfigurationError("Value should a valid integer")
     if converted_value < 1:
         raise ConfigurationError("Value should be greater than 0")
-
-
-def validate_new_income_sources(value: str) -> None:
-    message = "\n".join(
-        (
-            "Invalid format. All configurations should match match next pattern:",
-            CODE.format(text="value,value,value"),
-            "",
-            "Spaces not allowed between values. Use only comma.",
-            "",
-            "Example:",
-            BOLD.format(text="My new job,Design"),
-        )
-    )
-    if ", " in value:
-        raise ConfigurationError(message)
+    elif converted_value > 31:
+        raise ConfigurationError("Value should be less than 31")
 
 
 def configurations_validator_dispatcher(storage: ConfigurationsStorage) -> Callable[[str], None]:
     if storage.configuration_name == Configurations.KEYBOARD_DATES_AMOUNT.name.lower():
         return validate_new_dates_keyboard_len
-    elif storage.configuration_name == Configurations.INCOME_SOURCES.name.lower():
-        return validate_new_income_sources
 
     return lambda _: None
